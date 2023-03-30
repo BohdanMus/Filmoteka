683db100daa7402e5f852896c71dd3196ae1edeb
Stepan Skichko <st.skichko@gmail.com>
2023-03-30 21:24:58 +0300

    copied my code from another repo

---
+++ src/js/search-by-keyword.js
@@ -0,0 +1,101 @@
+//------------------------------------------------------------------------------------------------
+import axios from 'axios';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const USER_KEY = '9e4f0ad78cbe1148a9d4c0c8389afc83';
+export const prePoster = 'https://image.tmdb.org/t/p/original/';
+export const galleryEl = document.querySelector('.gallery-list');
+//------------------------------------------------------------------------------------------------
+import Pagination from 'tui-pagination';
+
+const refs = {
+  formRef: document.getElementById('search-form'),
+  galleryRef: document.querySelector('.gallery'),
+};
+
+refs.formRef.addEventListener('submit', onSubmitBtn);
+
+const container = document.getElementById('tui-pagination-container');
+
+function onSubmitBtn(e) {
+  e.preventDefault();
+  const keyWord = e.target.elements.searchQuery.value.trim();
+
+  // getMovieByName(keyWord);
+  getMovie(keyWord).then(data => {
+    // по даті 0 малюєм пагінацію
+    const instance = new Pagination(container, {
+      totalItems: data[0],
+      itemsPerPage: 20,
+      visiblePages: 5,
+    });
+
+    refs.galleryRef.insertAdjacentHTML('beforeend', galleryMarkup(data[1]));
+    // по даті 1 малюєм картку
+    console.log('масив на 1й сторінці', data[1]);
+
+    // слухач на пагінацію
+    instance.on('beforeMove', event => {
+      const currentPage = event.page;
+
+      // робимо запит мо тому самому keyWord тільки змінюємо сторінки
+      getMovie(keyWord, currentPage).then(data => {
+        console.log('масив на вибраній сторінці', data);
+      });
+    });
+  });
+}
+
+async function getMovie(name, page = 1) {
+  try {
+    // Створюєм запит по ключовому слову на сервер
+    const movies = await axios.get(
+      `${BASE_URL}/search/movie?api_key=$USER_KEY}&language=en-US&page=${page}&include_adult=false&query=${name}`
+    );
+
+    // Створюєм запит на сервер для отримання всіх жанрів
+    const genres = await axios.get(
+      `${BACE_URL}//genre/movie/list?api_key=${USER_KEY}&language=en-US`
+    );
+
+    // Масиви з кінами та жанрами
+    const movieArr = movies.data.results;
+    const genresArr = genres.data.genres;
+
+    // перебираємо масив кін, підставляємо в масив з жанрами замість чисел - відповідні їм жанри
+    const updatedMovies = movieArr.map(movie => {
+      const updatedGenreName = movie.genre_ids.map(genreId => {
+        const genre = genresArr.find(genre => genre.id === genreId);
+        return genre.name;
+      });
+
+      return {
+        ...movie,
+        genre_ids: updatedGenreName,
+      };
+    });
+
+    // console.log('мап', updatedMovies);
+    console.log('чистий бекенд', movies.data);
+    console.log('масив з кінами', movieArr);
+    console.log('масив з жанрами', genresArr);
+    if (page === 1) {
+      return [movies.data.total_results, updatedMovies];
+    } else {
+      return updatedMovies;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function galleryMarkup(data) {
+  const dataMarkup = data
+    .map(item => {
+      return `<div class="photo-card">
+                <img src="https://image.tmdb.org/t/p/original/${item.poster_path}" alt=""/>
+              </div>`;
+    })
+    .join('');
+
+  return dataMarkup;
+}
