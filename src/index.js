d136d34a54eaae3877d114e4c7d30c6dad4fd229
Stepan Skichko <st.skichko@gmail.com>
2023-04-01 16:44:58 +0300

    open&close modal team FIXED

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import { page } from './js/pagination';
 import { showSpinner, hideSpinner } from './js/spinner';
 import { onSearchMovieBtnClick } from './js/search-by-keyword';
 import './js/scroll-up-btn';
+import { onOpenModalTeamClick } from './js/team-modal';
 
 const refs = {
   formRef: document.querySelector('.header-search-form'),
+  openModalTeamBtn: document.getElementById('open-modal-btn'),
 };
 
 galleryRender(page);
 
+// Запускає пошук фільмів зв ключовим словом
 refs.formRef.addEventListener('submit', onSearchMovieBtnClick);
+
+// Обробник події для відкриття модального вікна з командою
+refs.openModalTeamBtn.addEventListener('click', onOpenModalTeamClick);
--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -1,25 +1,39 @@
-// Отримати кнопку відкриття модального вікна
-var openModalBtn = document.getElementById("open-modal-btn");
-
 // Отримати модальне вікно
-var modal = document.getElementById("modal");
+const modal = document.getElementById('modal-team');
 
 // Отримати кнопку закриття
-var closeBtn = document.getElementsByClassName("close")[0];
+const closeBtn = document.querySelector('.close');
 
-// Обробник події для відкриття модального вікна
-openModalBtn.onclick = function () {
-  modal.style.display = "block";
-}
+export function onOpenModalTeamClick() {
+  modal.classList.remove('is-hidden');
 
-// Обробник події для закриття модального вікна
-closeBtn.onclick = function() {
-  modal.style.display = "none";
+  closeBtn.addEventListener('click', e => {
+    modal.classList.add('is-hidden');
+  });
 }
 
 // Обробник події для закриття модального вікна, якщо користувач клікне поза ним
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.classList.add('is-hidden');
   }
-}
+};
+
+// // Отримати кнопку відкриття модального вікна
+// var openModalBtn = document.getElementById('open-modal-btn');
+
+// // Отримати модальне вікно
+// var modal = document.getElementById('modal');
+
+// // Отримати кнопку закриття
+// var closeBtn = document.getElementsByClassName('close')[0];
+
+// // Обробник події для відкриття модального вікна
+// openModalBtn.onclick = function () {
+//   modal.style.display = 'block';
+// };
+
+// // Обробник події для закриття модального вікна
+// closeBtn.onclick = function () {
+//   modal.style.display = 'none';
+// };
