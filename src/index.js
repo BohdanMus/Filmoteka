87e2f429095c9fb64f91e2ade926a8f602436334
Bivis60 <89808390+Bivis60@users.noreply.github.com>
2023-04-01 17:55:51 +0300

    Добув ел-ти фыльма

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { page } from './js/pagination';
 import { showSpinner, hideSpinner } from './js/spinner';
 import { onSearchMovieBtnClick } from './js/search-by-keyword';
 import './js/scroll-up-btn';
+import './js/open-modal';
 
 const refs = {
   formRef: document.querySelector('.header-search-form'),
