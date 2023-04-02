70dee1a694cda021e8480e36931a5d134cb89695
sides1 <dima.litvinenko1990@gmail.com>
2023-04-02 22:35:37 +0300

    added check for trailer

--- src/js/youtube-trailer.js
+++ src/js/youtube-trailer.js
@@ -1,28 +1,44 @@
 import axios from 'axios';
+import { Notify } from 'notiflix';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 const USER_KEY = '9e4f0ad78cbe1148a9d4c0c8389afc83';
+
 let trailerKey = '';
 
+const gallery = document.querySelector('.gallery-list');
 const trailerButton = document.querySelector('.trailer');
 const trailerContainer = document.querySelector('.trailer-container');
 
 trailerButton.addEventListener('click', getMovieTrailer);
+gallery.addEventListener('click', getMovieId);
 
 async function getMovieTrailer(movieId) {
   try {
     const response = await axios.get(
       `${BASE_URL}/movie/${movieId}/videos?api_key=${USER_KEY}`
     );
-    movieId = response.data.id;
+
     const videos = response.data.results;
     console.log(videos);
     trailerKey = videos.find(video => video.type === 'Trailer')?.key;
-    renderVideoFrame();
-    return response;
+    if (videos.length > 0) {
+      renderVideoFrame();
+    } else {
+      Notify.info('Sorry, trailer not found!');
+    }
   } catch (error) {
     console.error(error);
   }
+}
+
+function getMovieId(event) {
+  trailerContainer.innerHTML = '';
+
+  const movie = event.target.closest('li');
+  const movieId = movie.dataset.id;
+  console.log(movieId);
+  getMovieTrailer(movieId);
 }
 
 function renderVideoFrame() {
