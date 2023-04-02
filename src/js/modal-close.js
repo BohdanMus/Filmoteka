46d9d5b15423cc179a88ff84a06d8bc62866a40d
Maryna Danyliuk <maruna.danulyk@gmail.com>
2023-04-02 12:28:59 +0300

    js-modal-close and header-icon

---
+++ src/js/modal-close.js
@@ -0,0 +1,16 @@
+// const modalEl = document.querySelector(# тут id  або клас всєї модалки);
+// const modalBtnClose = document.querySelector(# клас хрестика закриття модалки);
+
+// const onCloseModal = (e) => {
+//     if (event.currentTarget === event.target) {
+//         modalEL.style.display = 'none';
+//     }
+// }
+
+// modalEl.addEventListener('click', OnCloseModal);
+// modalBtnClose.addEventListener('click', OnCloseModal);
+// document.removeEventListener('keydown', (e) => {
+//     if (e.key === 'Escape') {
+//          modalEL.style.display = 'none';
+//     }
+// })
