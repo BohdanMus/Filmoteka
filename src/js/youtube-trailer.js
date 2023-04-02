2091f40d49a8a59f8d6bd91aaeced31b56d52277
sides1 <dima.litvinenko1990@gmail.com>
2023-04-02 12:15:46 +0300

    code refactoring

--- src/js/youtube-trailer.js
+++ src/js/youtube-trailer.js
@@ -1,30 +1,36 @@
 import axios from 'axios';
 
 const apiKey = '9e4f0ad78cbe1148a9d4c0c8389afc83';
-const movieId = '726759';
+const movieId = '677179';
+let trailerKey = '';
 
 const trailerButton = document.querySelector('.trailer');
 const trailerContainer = document.querySelector('.trailer-container');
 
-trailerButton.addEventListener('click', async () => {
+trailerButton.addEventListener('click', getMovieTrailer);
+
+async function getMovieTrailer() {
   try {
     const response = await axios.get(
       `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`
     );
 
     const videos = response.data.results;
-    console.log(videos);
-    const trailerKey = videos.find(video => video.type === 'Trailer')?.key;
-
-    const iframe = document.createElement('iframe');
-    iframe.src = `https://www.youtube.com/embed/${trailerKey}`;
-    iframe.width = '560';
-    iframe.height = '315';
-    iframe.allowFullscreen = true;
-
-    trailerContainer.innerHTML = '';
-    trailerContainer.appendChild(iframe);
+    // console.log(videos);
+    trailerKey = videos.find(video => video.type === 'Trailer')?.key;
+    renderVideoFrame();
   } catch (error) {
     console.error(error);
   }
-});
+}
+
+function renderVideoFrame() {
+  const iframe = document.createElement('iframe');
+  iframe.src = `https://www.youtube.com/embed/${trailerKey}`;
+  iframe.width = '560';
+  iframe.height = '315';
+  iframe.allowFullscreen = true;
+
+  trailerContainer.innerHTML = '';
+  trailerContainer.appendChild(iframe);
+}
