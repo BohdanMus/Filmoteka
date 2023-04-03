31a18a7b8ffc96bef58ec6ad1e9551e8f40212b5
BohdanMus <112251293+BohdanMus@users.noreply.github.com>
2023-04-03 11:07:19 +0000

    Deploying to gh-pages from @ BohdanMus/Filmoteka@c073d85611487c22376f5cfb3c0f663b1b60863f 🚀

---
+++ library.320a8777.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("modal-team"),n=document.querySelector(".close");window.onclick=function(n){n.target==e&&e.classList.add("is-hidden")};({openModalTeamBtn:document.getElementById("open-modal-btn")}).openModalTeamBtn.addEventListener("click",(function(){e.classList.remove("is-hidden"),n.addEventListener("click",(n=>{e.classList.add("is-hidden")}))}));
+//# sourceMappingURL=library.320a8777.js.map
---
+++ library.ec3ebe18.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("modal-team"),n=document.querySelector(".close");window.onclick=function(n){n.target==e&&e.classList.add("is-hidden")},{openModalTeamBtn:document.getElementById("open-modal-btn")}.openModalTeamBtn.addEventListener("click",(function(){e.classList.remove("is-hidden"),n.addEventListener("click",(function(n){e.classList.add("is-hidden")}))}))}();
+//# sourceMappingURL=library.ec3ebe18.js.map
