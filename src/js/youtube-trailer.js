3a2a9f1454abba74b14c07973e96edc429197bdb
Dmytro Litvinenko <dima.litvinenko1990@gmail.com>
2023-04-08 21:13:53 +0300

    add trailer

--- src/js/youtube-trailer.js
+++ src/js/youtube-trailer.js
@@ -1,7 +1,6 @@
-
 import axios from 'axios';
 import { Notify } from 'notiflix';
-import {BASE_URL, USER_KEY} from "./API"
+import { BASE_URL, USER_KEY } from './API';
 
 const gallery = document.querySelector('.gallery-list');
 // const trailerContainer = document.querySelector('.trailer-container');
@@ -15,26 +14,22 @@ export async function getMovieTrailer(movieID) {
     );
 
     const videos = response.data.results;
-    console.log(videos);
     const trailerKey = videos.find(video => video.type === 'Trailer')?.key;
     if (trailerKey) {
       renderVideoFrame(trailerKey);
-    } else {
-      Notify.info('Sorry, trailer not found!');
     }
   } catch (error) {
-    console.error(error);
+    Notify.info('Sorry, trailer not found!');
   }
 }
-
 
 function renderVideoFrame(trailerKey) {
   const iframe = document.createElement('iframe');
   iframe.src = `https://www.youtube.com/embed/${trailerKey}`;
-  iframe.width = '500';
-  iframe.height = '315';
+  iframe.width = '';
+  iframe.height = '';
   iframe.allowFullscreen = true;
-   const trailerContainer = document.querySelector('.trailerContainer')
+  const trailerContainer = document.querySelector('.trailerContainer');
   trailerContainer.innerHTML = '';
   trailerContainer.appendChild(iframe);
 }
